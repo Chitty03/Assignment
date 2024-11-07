215e51064b43c35d03e1b0eb48032f5b8a8619c2
Chitty03 <chittysanjay36@gmail.com>
2024-11-07 15:09:55 +0800

    /

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let dataLoaded = false;
 let globalData = [];
 
-// Load CSV data with the exact column headers
+// Load CSV data with exact column headers
 function loadData(callback) {
   if (!dataLoaded) {
     d3.csv("Combined_Data.csv", d => ({
@@ -12,19 +12,30 @@ function loadData(callback) {
     })).then(data => {
       globalData = data;
       dataLoaded = true;
-      console.log("Data loaded:", globalData);  // Verify data is loaded correctly
       callback();
     }).catch(error => {
-      console.error("Error loading data:", error);  // Catch and log data loading errors
+      console.error("Error loading data:", error);
     });
   } else {
     callback();
   }
 }
 
-// Choropleth Map: Color-coded by Gini Index, tooltips with all data
+// Helper function for tooltips
+function showTooltip(content, event) {
+  d3.select(".tooltip")
+    .style("left", (event.pageX + 10) + "px")
+    .style("top", (event.pageY - 10) + "px")
+    .style("display", "inline-block")
+    .html(content);
+}
+
+function hideTooltip() {
+  d3.select(".tooltip").style("display", "none");
+}
+
+// Choropleth Map
 function createChoroplethMap() {
-  console.log("createChoroplethMap called");  // Debugging function call
   loadData(() => {
     const width = 800, height = 500;
     const svg = d3.select("#mapChart").append("svg").attr("width", width).attr("height", height);
@@ -43,20 +54,23 @@ function createChoroplethMap() {
          })
          .attr("stroke", "black")
          .attr("stroke-width", 0.5)
-         .append("title")
-         .text(d => {
+         .on("mousemove", function(event, d) {
            const countryData = globalData.find(c => c.Country === d.properties.name);
-           return countryData 
-             ? `Country: ${countryData.Country}\nGini Index: ${countryData.Gini_Index}\nLife Expectancy: ${countryData.Life_Expectancy}\nInfant Mortality: ${countryData.Infant_Mortality}`
-             : "No Data";
-         });
-    }).catch(error => console.error("Error loading GeoJSON data:", error));
+           if (countryData) {
+             const tooltipContent = `<strong>Country:</strong> ${countryData.Country}<br>
+                                     <strong>Gini Index:</strong> ${countryData.Gini_Index}<br>
+                                     <strong>Life Expectancy:</strong> ${countryData.Life_Expectancy}<br>
+                                     <strong>Infant Mortality:</strong> ${countryData.Infant_Mortality}`;
+             showTooltip(tooltipContent, event);
+           }
+         })
+         .on("mouseout", hideTooltip);
+    });
   });
 }
 
-// Bar Chart: Bars for Life Expectancy, color-coded by Gini Index, tooltips with all data
+// Bar Chart
 function createBarChart() {
-  console.log("createBarChart called");  // Debugging function call
   loadData(() => {
     const svg = d3.select("#barChart").append("svg").attr("width", 800).attr("height", 500);
     const x = d3.scaleBand().domain(globalData.map(d => d.Country)).range([0, 800]).padding(0.1);
@@ -71,23 +85,47 @@ function createBarChart() {
        .attr("width", x.bandwidth())
        .attr("height", d => 500 - y(d.Life_Expectancy))
        .attr("fill", d => colorScale(d.Gini_Index))
-       .append("title")
-       .text(d => `Country: ${d.Country}\nGini Index: ${d.Gini_Index}\nLife Expectancy: ${d.Life_Expectancy}\nInfant Mortality: ${d.Infant_Mortality}`);
+       .on("mousemove", (event, d) => {
+         const tooltipContent = `<strong>Country:</strong> ${d.Country}<br>
+                                 <strong>Gini Index:</strong> ${d.Gini_Index}<br>
+                                 <strong>Life Expectancy:</strong> ${d.Life_Expectancy}<br>
+                                 <strong>Infant Mortality:</strong> ${d.Infant_Mortality}`;
+         showTooltip(tooltipContent, event);
+       })
+       .on("mouseout", hideTooltip);
 
-    svg.append("g").attr("transform", "translate(0,500)").call(d3.axisBottom(x)).selectAll("text").attr("transform", "rotate(-40)").style("text-anchor", "end");
-    svg.append("g").call(d3.axisLeft(y));
+    svg.append("g")
+       .attr("transform", "translate(0,500)")
+       .call(d3.axisBottom(x).tickFormat(d => d).ticks(5))
+       .selectAll("text")
+       .attr("transform", "rotate(-45)")
+       .style("text-anchor", "end");
+
+    svg.append("g")
+       .call(d3.axisLeft(y));
+
+    svg.append("text")
+       .attr("class", "axis-label")
+       .attr("x", -250)
+       .attr("y", 20)
+       .attr("transform", "rotate(-90)")
+       .text("Life Expectancy (Years)");
+
+    svg.append("text")
+       .attr("class", "axis-label")
+       .attr("x", 400)
+       .attr("y", 540)
+       .text("Country");
   });
 }
 
-// Scatter Plot: Gini Index vs Life Expectancy, bubble size for Infant Mortality, color-coded by Gini Index
+// Scatter Plot
 function createScatterPlot() {
-  console.log("createScatterPlot called");  // Debugging function call
   loadData(() => {
     const svg = d3.select("#scatterPlot").append("svg").attr("width", 800).attr("height", 500);
     const x = d3.scaleLinear().domain([0.2, 0.5]).range([0, 800]);
     const y = d3.scaleLinear().domain([70, 90]).range([500, 0]);
     const sizeScale = d3.scaleSqrt().domain([0, 10]).range([5, 20]);
-    const colorScale = d3.scaleSequential(d3.interpolateBlues).domain([0.2, 0.5]);
 
     svg.selectAll("circle")
        .data(globalData)
@@ -95,59 +133,107 @@ function createScatterPlot() {
        .attr("cx", d => x(d.Gini_Index))
        .attr("cy", d => y(d.Life_Expectancy))
        .attr("r", d => sizeScale(d.Infant_Mortality))
-       .attr("fill", d => colorScale(d.Gini_Index))
-       .append("title")
-       .text(d => `Country: ${d.Country}\nGini Index: ${d.Gini_Index}\nLife Expectancy: ${d.Life_Expectancy}\nInfant Mortality: ${d.Infant_Mortality}`);
+       .attr("fill", "#69b3a2")
+       .on("mousemove", (event, d) => {
+         const tooltipContent = `<strong>Country:</strong> ${d.Country}<br>
+                                 <strong>Gini Index:</strong> ${d.Gini_Index}<br>
+                                 <strong>Life Expectancy:</strong> ${d.Life_Expectancy}<br>
+                                 <strong>Infant Mortality:</strong> ${d.Infant_Mortality}`;
+         showTooltip(tooltipContent, event);
+       })
+       .on("mouseout", hideTooltip);
 
-    svg.append("g").attr("transform", "translate(0,500)").call(d3.axisBottom(x).ticks(5));
-    svg.append("g").call(d3.axisLeft(y).ticks(5));
+    svg.append("g")
+       .attr("transform", "translate(0,500)")
+       .call(d3.axisBottom(x).tickFormat(d3.format(".2f")));
+
+    svg.append("g")
+       .call(d3.axisLeft(y).ticks(5));
+
+    svg.append("text")
+       .attr("class", "axis-label")
+       .attr("x", -250)
+       .attr("y", 20)
+       .attr("transform", "rotate(-90)")
+       .text("Life Expectancy (Years)");
+
+    svg.append("text")
+       .attr("class", "axis-label")
+       .attr("x", 400)
+       .attr("y", 540)
+       .text("Gini Index");
   });
 }
 
-// Line Chart: Line for Infant Mortality, color of points based on Life Expectancy, tooltips with all data
+// Line Chart
 function createLineChart() {
-  console.log("createLineChart called");  // Debugging function call
-  loadData(() => {
-    const svg = d3.select("#lineChart").append("svg").attr("width", 800).attr("height", 500);
-    const x = d3.scalePoint().domain(globalData.map(d => d.Country)).range([0, 800]);
-    const y = d3.scaleLinear().domain([0, d3.max(globalData, d => d.Infant_Mortality)]).range([500, 0]);
-    const colorScale = d3.scaleSequential(d3.interpolateBlues).domain([70, 90]);
-
-    const line = d3.line()
-                  .x(d => x(d.Country))
-                  .y(d => y(d.Infant_Mortality));
-
-    svg.append("path")
-       .datum(globalData)
-       .attr("fill", "none")
-       .attr("stroke", "blue")
-       .attr("stroke-width", 2)
-       .attr("d", line);
-
-    svg.selectAll("circle")
-       .data(globalData)
-       .enter().append("circle")
-       .attr("cx", d => x(d.Country))
-       .attr("cy", d => y(d.Infant_Mortality))
-       .attr("r", 4)
-       .attr("fill", d => colorScale(d.Life_Expectancy))
-       .append("title")
-       .text(d => `Country: ${d.Country}\nInfant Mortality: ${d.Infant_Mortality}\nLife Expectancy: ${d.Life_Expectancy}\nGini Index: ${d.Gini_Index}`);
-
-    svg.append("g").attr("transform", "translate(0,500)").call(d3.axisBottom(x)).selectAll("text").attr("transform", "rotate(-40)").style("text-anchor", "end");
-    svg.append("g").call(d3.axisLeft(y).ticks(5));
-  });
-}
-
-// Dual-Axis Chart for Life Expectancy and Infant Mortality, tooltips with all data
-function createDualAxisChart() {
-    console.log("createDualAxisChart called");  // Debugging function call
+    loadData(() => {
+      const svg = d3.select("#lineChart").append("svg").attr("width", 800).attr("height", 500);
+      const x = d3.scalePoint().domain(globalData.map(d => d.Country)).range([0, 800]);
+      const y = d3.scaleLinear().domain([0, d3.max(globalData, d => d.Infant_Mortality)]).range([500, 0]);
+  
+      const line = d3.line()
+                     .x(d => x(d.Country))
+                     .y(d => y(d.Infant_Mortality));
+  
+      // Draw the line
+      svg.append("path")
+         .datum(globalData)
+         .attr("fill", "none")
+         .attr("stroke", "blue")
+         .attr("stroke-width", 2)
+         .attr("d", line);
+  
+      // Add circles for each point
+      svg.selectAll("circle")
+         .data(globalData)
+         .enter().append("circle")
+         .attr("cx", d => x(d.Country))
+         .attr("cy", d => y(d.Infant_Mortality))
+         .attr("r", 4)
+         .attr("fill", "black")
+         .on("mousemove", (event, d) => {
+           const tooltipContent = `<strong>Country:</strong> ${d.Country}<br>
+                                   <strong>Infant Mortality:</strong> ${d.Infant_Mortality}<br>
+                                   <strong>Life Expectancy:</strong> ${d.Life_Expectancy}<br>
+                                   <strong>Gini Index:</strong> ${d.Gini_Index}`;
+           showTooltip(tooltipContent, event);
+         })
+         .on("mouseout", hideTooltip);
+  
+      // X-axis and Y-axis
+      svg.append("g")
+         .attr("transform", "translate(0,500)")
+         .call(d3.axisBottom(x))
+         .selectAll("text")
+         .attr("transform", "rotate(-45)")
+         .style("text-anchor", "end");
+  
+      svg.append("g")
+         .call(d3.axisLeft(y).ticks(5));
+  
+      svg.append("text")
+         .attr("class", "axis-label")
+         .attr("x", -250)
+         .attr("y", 20)
+         .attr("transform", "rotate(-90)")
+         .text("Infant Mortality (per 1000 live births)");
+  
+      svg.append("text")
+         .attr("class", "axis-label")
+         .attr("x", 400)
+         .attr("y", 540)
+         .text("Country");
+    });
+  }
+  
+  // Dual-Axis Chart for Life Expectancy and Infant Mortality
+  function createDualAxisChart() {
     loadData(() => {
       const svg = d3.select("#dualAxisChart").append("svg").attr("width", 800).attr("height", 500);
       const x = d3.scaleBand().domain(globalData.map(d => d.Country)).range([0, 800]).padding(0.1);
       const yLeft = d3.scaleLinear().domain([70, 90]).range([500, 0]);  // Left y-axis for Life Expectancy
       const yRight = d3.scaleLinear().domain([0, d3.max(globalData, d => d.Infant_Mortality)]).range([500, 0]);  // Right y-axis for Infant Mortality
-      const colorScale = d3.scaleSequential(d3.interpolateBlues).domain([0.2, 0.5]);
   
       // Bars for Life Expectancy, color-coded by Gini Index
       svg.selectAll(".bar")
@@ -157,9 +243,15 @@ function createDualAxisChart() {
          .attr("y", d => yLeft(d.Life_Expectancy))
          .attr("width", x.bandwidth())
          .attr("height", d => 500 - yLeft(d.Life_Expectancy))
-         .attr("fill", d => colorScale(d.Gini_Index))
-         .append("title")
-         .text(d => `Country: ${d.Country}\nLife Expectancy: ${d.Life_Expectancy}\nGini Index: ${d.Gini_Index}\nInfant Mortality: ${d.Infant_Mortality}`);
+         .attr("fill", "#4a90e2")
+         .on("mousemove", (event, d) => {
+           const tooltipContent = `<strong>Country:</strong> ${d.Country}<br>
+                                   <strong>Life Expectancy:</strong> ${d.Life_Expectancy}<br>
+                                   <strong>Gini Index:</strong> ${d.Gini_Index}<br>
+                                   <strong>Infant Mortality:</strong> ${d.Infant_Mortality}`;
+           showTooltip(tooltipContent, event);
+         })
+         .on("mouseout", hideTooltip);
   
       // Circles for Infant Mortality on the right y-axis
       svg.selectAll(".circle")
@@ -169,21 +261,26 @@ function createDualAxisChart() {
          .attr("cy", d => yRight(d.Infant_Mortality))
          .attr("r", 5)
          .attr("fill", "red")
-         .append("title")
-         .text(d => `Country: ${d.Country}\nInfant Mortality: ${d.Infant_Mortality}\nGini Index: ${d.Gini_Index}\nLife Expectancy: ${d.Life_Expectancy}`);
+         .on("mousemove", (event, d) => {
+           const tooltipContent = `<strong>Country:</strong> ${d.Country}<br>
+                                   <strong>Infant Mortality:</strong> ${d.Infant_Mortality}<br>
+                                   <strong>Life Expectancy:</strong> ${d.Life_Expectancy}<br>
+                                   <strong>Gini Index:</strong> ${d.Gini_Index}`;
+           showTooltip(tooltipContent, event);
+         })
+         .on("mouseout", hideTooltip);
   
-      // X-Axis for Country Names
+      // X-Axis
       svg.append("g")
          .attr("transform", "translate(0,500)")
          .call(d3.axisBottom(x))
          .selectAll("text")
-         .attr("transform", "rotate(-40)")
+         .attr("transform", "rotate(-45)")
          .style("text-anchor", "end");
   
       // Left Y-Axis for Life Expectancy
       svg.append("g")
          .call(d3.axisLeft(yLeft).ticks(5))
-         .attr("class", "y-axis-left")
          .append("text")
          .attr("fill", "black")
          .attr("transform", "rotate(-90)")
@@ -197,7 +294,6 @@ function createDualAxisChart() {
       svg.append("g")
          .attr("transform", "translate(800,0)")
          .call(d3.axisRight(yRight).ticks(5))
-         .attr("class", "y-axis-right")
          .append("text")
          .attr("fill", "black")
          .attr("transform", "rotate(-90)")
